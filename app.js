f8f846a760bd1d8fe95cc0f9c53b16fdae5fb978
YuriiRogovets <yura.rogovets@gmail.com>
2024-05-26 01:36:58 +0300

    created user model

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/users", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
