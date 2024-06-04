fcad872050360e5353042d6bf59168335c8402f4
YuriiRogovets <yura.rogovets@gmail.com>
2024-06-04 16:32:00 +0300

    added mentors comments

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,22 +2,26 @@ import * as fs from "node:fs/promises";
 import path from "node:path";
 import User from "../models/user.js"
 import Jimp from "jimp";
+import HttpError from "../helpers/HttpError.js"
 
 
 async function changeAvatar(req, res, next) {
     try {
+         
+        if (!req.file) {
+            throw HttpError(400);
+        }
 
         const newPath = path.resolve("public", "avatars", req.file.filename);
 
         (await Jimp.read(req.file.path)).resize(250, 250).write(req.file.path);
 
         await fs.rename(req.file.path, newPath);
-        console.log(req.user);
 
         const updatedUser = await User.findByIdAndUpdate(req.user.id, { avatarURL: req.file.filename }, { new: true });
 
         const feedbackMessage = {
-            avatarURL: updatedUser.avatarURL
+            avatarURL: `/avatars/${updatedUser.avatarURL}`
             };
 
         res.send(feedbackMessage);
